53fafaf9c3c4f241251f94a7ce4cb2a86b651c02
dementeddr <2413901+dementeddr@users.noreply.github.com>
2024-02-07 15:47:09 -0500

    Added a minecraft gaming layer, with some attempts at Minihud and Tweakeroo binding

--- keyboards/crkbd/keymaps/dementeddr/keymap.c
+++ keyboards/crkbd/keymaps/dementeddr/keymap.c
@@ -7,6 +7,7 @@ enum custom_layers {
     NAVI, // navigation
     MOUS, // mouse buttons
     FUNC, // function keys
+    MINE, // minecraft/gaming
 };
 
 enum custom_keycodes {
@@ -14,6 +15,7 @@ enum custom_keycodes {
     VRSN,
     RGB_SLD,
     CS_LAMBDA,
+    GAME_T,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -25,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_LCTL, LCTL_T(KC_Z), KC_X,  KC_C,  KC_V,   KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH),  KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                MO(1), LT(1,KC_BACKSPACE), LT(3,KC_TAB), KC_ENT, LT(2,KC_SPC),   MO(4)
+                                     MO(SYMB), KC_BACKSPACE, LT(MOUS,KC_TAB), KC_ENT, LT(NAVI,KC_SPC),   MO(FUNC)
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -37,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_LCTL, LCTL_T(KC_PERC), KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,              KC_PLUS, KC_1,    KC_2,    KC_3,   RCTL_T(KC_DOT), KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_NO,  KC_BACKSPACE, KC_TAB,  KC_ENT,  KC_0,    KC_0
+                                        KC_NO,  KC_BACKSPACE, KC_TAB,  KC_ENT,  KC_SPC,  KC_0
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -55,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [MOUS] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      VRSN,   KC_NO,   KC_NO,   KC_NO,   RGB_SLD, KC_VOLU,                      KC_NO,   KC_WH_L, KC_MS_U, KC_WH_R, KC_NO,   KC_NO,
+      TO(MINE),   KC_NO,   KC_NO,   KC_NO,   RGB_SLD, KC_VOLU,                      KC_NO,   KC_WH_L, KC_MS_U, KC_WH_R, KC_NO,   VRSN,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_ESC, KC_LSFT, KC_LALT, KC_LGUI,  KC_NO,   KC_VOLD,                     KC_NO,   KC_MS_R, KC_MS_D, KC_MS_L, KC_ACL2, KC_ENT,
+      KC_ESC, KC_LSFT, KC_LALT, KC_LGUI,  KC_NO,   KC_VOLD,                     KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_ACL2, KC_ENT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, KC_LCTL, KC_NO,  KC_NO,   KC_NO,   KC_MUTE,                      KC_NO,   KC_WH_D, KC_WH_U, KC_NO,   KC_RCTL, KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -74,6 +76,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LCTL, KC_LCTL, KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12,  KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          KC_NO, KC_BACKSPACE, KC_TAB,  KC_NO,   KC_NO,   KC_NO
+                                      //`--------------------------'  `--------------------------'
+  ),
+    
+    [MINE] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+     KC_TAB,   KC_M,    KC_Q,   KC_W,    KC_E,    GAME_T,                       TO(0),   TO(0),  TO(0),   TO(0),   TO(0),  TO(0),
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+     KC_ESC,  KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                         TO(0),   TO(0),  TO(0),   TO(0),   TO(0),  TO(0),
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+     KC_LCTL, KC_LCTL, KC_X,    KC_C,    KC_NO,   KC_B,                         TO(0),   TO(0),  TO(0),   TO(0),   TO(0),  TO(0),
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                         KC_K,   KC_SPACE, KC_H,       TO(0),   TO(0),   TO(0) 
                                       //`--------------------------'  `--------------------------'
   )
 };
@@ -104,6 +118,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case CS_LAMBDA:
         if (record->event.pressed) {
             SEND_STRING(" => ");
+        }
+        break;
+    case GAME_T:
+        if (record->event.pressed) {
+            tap_code16(TO(0));
+            tap_code16(KC_T);
         }
         break;
 
